4e7f60b52a831602058979e16c9c87ec9ba356de
Ha Phi Hung <haphihung55@gmail.com>
2023-05-25 22:47:57 +0700

    Implement sign-up

--- src/schema/user.ts
+++ src/schema/user.ts
@@ -1,11 +1,12 @@
 import { schemaBuilder } from '../schema-builder';
-import { Gender, UserType } from '@prisma/client';
+import { Gender, User, UserType } from '@prisma/client';
 import { ReviewSortBy } from './enums/review-sort-by';
 import { SortDirection } from './enums/sort-direction';
 import { createReviewsOrderByQuery } from './movie';
 import { prismaClient } from '../prisma-client';
+import bcrypt from 'bcrypt';
 
-schemaBuilder.prismaNode('User', {
+const User = schemaBuilder.prismaNode('User', {
   id: { field: 'id' },
   fields: (t) => ({
     username: t.exposeString('username'),
@@ -111,3 +112,59 @@ schemaBuilder.queryFields((t) => ({
       }),
   }),
 }));
+
+function userTypeDataCreationQuery(
+  userType: UserType,
+  blogUrl?: string | null
+) {
+  if (userType === UserType.Critic) {
+    if (!blogUrl) {
+      throw new Error('Critic user needs a blog or website URL');
+    }
+    return {
+      criticUser: { create: { blogUrl } },
+    };
+  }
+
+  return { regularUser: { create: {} } };
+}
+
+async function hashPassword(password: string): Promise<string> {
+  const salt = await bcrypt.genSalt(12);
+  return await bcrypt.hash(password, salt);
+}
+
+const SignUpInput = schemaBuilder.inputType('SignUpInput', {
+  fields: (t) => ({
+    username: t.string({ required: true }),
+    email: t.string({ required: true }),
+    name: t.string({ required: true }),
+    password: t.string({ required: true }),
+    dateOfBirth: t.field({ type: 'Date' }),
+    gender: t.field({ type: Gender }),
+    userType: t.field({ type: UserType, required: true }),
+    blogUrl: t.string(),
+  }),
+});
+
+schemaBuilder.mutationField('signUp', (t) =>
+  t.field({
+    type: User,
+    args: {
+      input: t.arg({ type: SignUpInput, required: true }),
+    },
+    resolve: async (_, args) =>
+      await prismaClient.user.create({
+        data: {
+          username: args.input.username,
+          name: args.input.name,
+          email: args.input.email,
+          dateOfBirth: args.input.dateOfBirth,
+          gender: args.input.gender,
+          userType: args.input.userType,
+          ...userTypeDataCreationQuery(args.input.userType, args.input.blogUrl),
+          hashedPassword: await hashPassword(args.input.password),
+        },
+      }),
+  })
+);
