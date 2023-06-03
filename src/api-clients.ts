2de8964edf6b6b94e77b85dfeb5a67ade7a34960
Ha Phi Hung <haphihung55@gmail.com>
2023-06-04 00:26:55 +0700

    Setup aws auth for production usage

--- src/api-clients.ts
+++ src/api-clients.ts
@@ -1,5 +1,5 @@
 import { S3Client } from '@aws-sdk/client-s3';
-import { fromSSO } from '@aws-sdk/credential-providers';
+import { fromIni, fromSSO } from '@aws-sdk/credential-providers';
 import { PrismaClient } from '@prisma/client';
 import { NoS3BucketError } from './errors';
 
@@ -8,4 +8,6 @@ export const prismaClient = new PrismaClient();
 if (!process.env.S3_BUCKET) {
   throw new NoS3BucketError();
 }
-export const s3Client = new S3Client({ credentials: fromSSO({}) });
+const credentials =
+  process.env.NODE_ENV === 'production' ? fromIni({}) : fromSSO({});
+export const s3Client = new S3Client({ credentials });
